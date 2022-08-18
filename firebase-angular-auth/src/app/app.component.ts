44ef32365e1c41593b51cbb0f485a6dec95b5e8b
sourabhsingh-cedcoss <sourabhsingh@cedcoss.com>
2022-08-18 18:18:05 +0530

    fixing the bug

--- firebase-angular-auth/src/app/app.component.ts
+++ firebase-angular-auth/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firebase-angular-auth';
+  isSignedIn = false;
+  constructor(public firebaseService: FirebaseService) {}
+  ngOnInit() {
+    this.firebaseService.signup('sourabhh@mail.com', '12345');
+    if (localStorage.getItem('user') !== null) {
+      this.isSignedIn = true;
+    } else {
+      this.isSignedIn = false;
+    }
+  }
+
+  async onSignup(email: string, password: string) {
+    await this.firebaseService.signup(email, password);
+
+    this.firebaseService.isLoggedIn === true && (this.isSignedIn = true);
+  }
+  async onSignin(email: string, password: string) {
+    await this.firebaseService.signin(email, password);
+
+    this.firebaseService.isLoggedIn === true && (this.isSignedIn = true);
+  }
+
+  handleLogout() {
+    this.isSignedIn = false;
+  }
 }
--- firebase-angular-auth/src/app/app.module.ts
+++ firebase-angular-auth/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyB-tq_dMJNIhHHR6u4FsnQjc2bz3MH93mg',
+      authDomain: 'test-4f90f.firebaseapp.com',
+      projectId: 'test-4f90f',
+      storageBucket: 'test-4f90f.appspot.com',
+      messagingSenderId: '57524428694',
+      appId: '1:57524428694:web:1b930c37389be74a2c87b7',
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FirebaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
